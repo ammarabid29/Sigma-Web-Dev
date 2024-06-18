380d47e26fa65c85d63325fffa9b6239eabaa393
ammarabid29 <aammarawan786@gmail.com>
2024-06-18 16:51:32 +0500

    57

---
+++ Javascript/Js-loops/index.js
@@ -0,0 +1,41 @@
+console.log("I am tutorial on loops")
+
+let a = 1;
+
+// for loop
+// for (let i = 0; i < 100; i++) {
+//     console.log(a + i);
+// }
+
+let obj = {
+    name: "Ammar",
+    role: "Programmer",
+    company: "CodeWithHarry AI"
+}
+
+// for in loop
+// it is for object 
+// for (const key in obj) {
+//         console.log(key , obj[key]);
+// }
+
+// for of loop
+// it is for array 
+// for (const c of "Ammar") {
+//     console.log(c);
+// }
+
+let i = 10;
+
+// while loop
+// while (i < 6) {
+//     console.log(i);
+//     i++;
+// }
+
+// do while loop
+// aik dafa lazmi chalta hai 
+do {
+    console.log(i);
+    i++;
+} while (i < 6);
