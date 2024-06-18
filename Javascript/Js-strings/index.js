3f61bcd05e15fb9d03f76a5dd9d2e373d3a2a2b4
ammarabid29 <aammarawan786@gmail.com>
2024-06-18 17:49:13 +0500

    60

---
+++ Javascript/Js-strings/index.js
@@ -0,0 +1,36 @@
+console.log("Javascript Strings");
+
+let a = "Harry";
+// console.log(a);
+// console.log(a[0]);
+// console.log(a[1]);
+// console.log(a[2]);
+// console.log(a[3]);
+// console.log(a[4]);
+
+// console.log(a.length);
+
+let real_name = "Ammar";
+let friend = "Abid";
+// console.log("His name is " + real_name + " and his friends name is " + friend);
+
+// Template Literals
+// console.log(`His name is ${real_name} and his "friends" name is ${friend}`);
+
+let b = "Ammar";
+
+// console.log(b.toUpperCase());
+// console.log(b.toLowerCase());
+
+// console.log(b.slice(1, 4));
+// console.log(b.slice(1));
+
+// console.log(b.replace("mm","77"));
+
+// console.log(b.concat(a , "Katrina" , "Rahul"));
+
+// let intro = "       My name is Ammar";
+// console.log(intro.trim());
+
+// string is immutable as remain same even applying so many functions 
+console.log(b);
