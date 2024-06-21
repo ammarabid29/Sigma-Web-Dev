8c3f7707020e0edbc22225962f0837fdaca63bb7
ammarabid29 <aammarawan786@gmail.com>
2024-06-22 00:55:17 +0500

    71

---
+++ Javascript/Js-remove-insert-elements/script.js
@@ -0,0 +1,70 @@
+// let container = document.querySelector(".container").innerHTML;
+// console.log(container);
+
+// let container = document.querySelector(".container").innerText;
+// console.log(container);
+
+// let container = document.querySelector(".container").outerHTML;
+// console.log(container);
+
+// let container = document.querySelector(".container").tagName;
+// console.log(container);
+
+// let container = document.querySelector(".container").nodeName;
+// console.log(container);
+
+// let container = document.querySelector(".container").textContent;
+// console.log(container);
+
+// let container = document.querySelector(".container").hidden = true;
+// console.log(container);
+
+// let box = document.querySelector(".box").innerHTML = "Hey I am Ammar";
+// console.log(box);
+
+// let box = document.querySelector(".box").hasAttribute("style");
+// console.log(box);
+
+// let box = document.querySelector(".box").getAttribute("style");
+// console.log(box);
+
+// let box = document.querySelector(".box").setAttribute("style","display: inline");
+
+// let box = document.querySelector(".box").attributes;
+// console.log(box);
+
+// let box = document.querySelector(".box").removeAttribute("style");
+
+// let box = document.querySelector(".box").dataset;
+// console.log(box);
+
+// change websites content on runtime 
+// document.designMode="on";
+
+// For adding nodes 
+// let div = document.createElement("div");
+// div.innerHTML="I have been inserted by <b>by Ammar</b>"
+// div.setAttribute("class","created")
+// document.querySelector(".container").append(div); //append add element on last
+// document.querySelector(".container").prepend(div); //prepand add element on first
+// document.querySelector(".container").before(div); //before add element before element not inside
+// document.querySelector(".container").after(div); //after add element after element not inside
+// document.querySelector(".container").replaceWith(div); //replaceWith replace the element
+
+// For adding HTML 
+// let cont = document.querySelector(".container");
+// cont.insertAdjacentHTML("beforebegin","<b>I am under the water. Help me.... iuuuoooo</b>")
+// cont.insertAdjacentText("afterbegin","I am under the water. Help me.... iuuuoooo")
+// let newElement = document.createElement("div");
+// newElement.setAttribute("class", "new")
+// newElement.innerHTML="I am new div"
+// cont.insertAdjacentElement("afterend", newElement)
+
+// let box2 = document.getElementById("box2");
+// box2.remove();
+
+// console.log(document.querySelector(".container").className);
+// console.log(document.querySelector(".container").classList);
+// document.querySelector(".container").classList.add("div1");
+// document.querySelector(".container").classList.remove("red");
+// document.querySelector(".container").classList.toggle("red"); //agar hai to remove kr do ni hai to add kr do
