3c822d0cdbc4fc0dd9f83fb49d48f5c4cbb1bae0
ammarabid29 <aammarawan786@gmail.com>
2024-09-30 16:43:32 +0500

    84b

---
+++ Spotify Clone/script.js
@@ -0,0 +1,44 @@
+console.log("lets write javascript")
+
+async function getSongs() {
+    let a = await fetch("http://127.0.0.1:3000/Spotify%20Clone/songs/")
+    let response = await a.text()
+    console.log(response)
+    let div = document.createElement("div")
+    div.innerHTML = response
+    let as = div.getElementsByTagName("a")
+    let songs = [];
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href.split("/songs/")[1]);
+        }
+    }
+    return songs
+
+}
+
+async function main() {
+    // get list of all songs 
+    let songs = await getSongs()
+    console.log(songs)
+
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li>
+                            <img class="invert" src="music.svg" alt="">
+                            <div class="info">
+                                <div>${song.replaceAll("%20", " ")}</div>
+                                <div>Harry</div>
+                            </div>
+                            <div class="playnow">
+                                <span>Play Now</span>
+                                <img class="invert" src="play.svg" alt="">
+                            </div>
+                          </li>`;
+    }
+
+   
+}
+
+main()
